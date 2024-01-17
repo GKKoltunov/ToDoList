0f4307a54cc902283d2156d8ae738e3951f5b33f
Мария <maria@iMac-Maria.local>
2024-01-17 16:09:20 +0300

    localSorage

--- src/app/app.js
+++ src/app/app.js
@@ -7,26 +7,35 @@ const add = document.querySelector(".add");
 const hidden = document.querySelector('.hidden')
 
 
+
+//заполняем массив todos
 let addTask = () => {
   if (input.value.trim().length) {
+    
     todos.push({
       text: input.value,
       id: Date.now(),
       isCompleted: false,
     });
+    window.localStorage.setItem(input.value , 0);
   }
 };
 
+// 
 let toggleIsCompleted = (id) => {
   const task = todos.find((el) => el.id === id);
   task.isCompleted = !task.isCompleted;
-  console.log(todos);
+  
 };
 
+// let check = (text) => {
+//   const arr = todos.find(el => el.text === text);
+//   localStorage.getItem(arr.text, 1);
+//   console.log(arr)
+// }
 
 
-
-
+// создания элемента списка
 let createTaskNode = (obj) => {
   let li = document.createElement("li");
   li.className = "elem";
@@ -37,10 +46,15 @@ let createTaskNode = (obj) => {
   // const btnCross = li.querySelector(".cross");
   // btnCross.addEventListener("click", function(){});
   checkbox.checked = obj.isCompleted;
-  checkbox.addEventListener("change", () => toggleIsCompleted(obj.id));
+  checkbox.addEventListener("change", () => {
+    toggleIsCompleted(obj.id)
+    // check(obj.text)
+  });
   return li;
 };
 
+
+// заполнение списка элементами
 let renderTasks = () => {
   ul.innerHTML = "";
   todos.forEach((el) => ul.append(createTaskNode(el)));
@@ -48,26 +62,53 @@ let renderTasks = () => {
 };
 
 
+
+// загрузка элементов из localStorage при запуске страницы
+let start = () => {
+  if (localStorage.length !== 0) {
+    let keys = Object.keys(localStorage);
+    for (let key of keys) {
+      todos.unshift({
+        text: key,
+        id: Date.now(),
+        isCompleted: false,
+      })
+    };
+    hidden.style.display = "block";
+    renderTasks();
+  }
+};
+start()
+
+
+
+
+// реакция на нажатие кнопки "добавить"
 add.addEventListener("click", function (event) {
      event.preventDefault();  // отменить дефолтное поведение
   hidden.style.display="block"
   addTask();
   renderTasks();
+  
   console.log(todos);
 });
 
 
 const deleteAll = document.querySelector(".delete-all");
 
+// реакция на нажатие кнопки "удалить все"
 deleteAll.addEventListener('click', function () {
   ul.innerHTML = "";
   todos = []
   hidden.style.display = "none";
+  localStorage.clear()
 })
 
 
 const deleteCmpl = document.querySelector(".delete-cmpl");
 
+
+// реакция на нажатие кнопки "удалить завершенные"
 deleteCmpl.addEventListener('click', function () {
   
   ul.innerHTML = "";
