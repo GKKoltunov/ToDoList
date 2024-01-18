134f7644126055292143c71779c7122d0058f983
Мария <maria@iMac-Maria.local>
2024-01-18 13:01:09 +0300

    local

--- src/app/app.js
+++ src/app/app.js
@@ -5,26 +5,34 @@ const list = document.querySelector(".list");
 const ul = document.querySelector(".ul");
 const add = document.querySelector(".add");
 const hidden = document.querySelector('.hidden')
+const deleteCmpl = document.querySelector(".delete-cmpl");
+const deleteAll = document.querySelector(".delete-all");
 
+function filllocalStorage() {
+  let json = JSON.stringify(todos);
+  window.localStorage.setItem("todo", json);
+}
 
 
 //заполняем массив todos
 let addTask = () => {
   if (input.value.trim().length) {
-    
     todos.push({
       text: input.value,
       id: Date.now(),
       isCompleted: false,
     });
-    window.localStorage.setItem(input.value , 0);
   }
+ filllocalStorage();
 };
 
 
+
 let toggleIsCompleted = (id) => {
+ 
   const task = todos.find((el) => el.id === id);
   task.isCompleted = !task.isCompleted;
+  filllocalStorage();
 };
 
 
@@ -32,69 +40,61 @@ let toggleIsCompleted = (id) => {
 
 
 // создания элемента списка
-let createTaskNode = (obj) => {
+function createTaskNode (obj) {
   let li = document.createElement("li");
   li.className = "elem";
   li.innerHTML = `<input class="checkbox" type="checkbox" name="" id=""></input>
   <p class="task">${obj.text}</p>
   <button class="cross">❌</button> `;
-  let checkbox = li.querySelector(".checkbox");
+  let checkbox = li.querySelector(".checkbox");  //реакция нажатия на чекбокс
   checkbox.checked = obj.isCompleted;
   checkbox.addEventListener("change", () => {
     toggleIsCompleted(obj.id)
-    window.localStorage.setItem(obj.text, +(checkbox.checked))
   });
 
-  let cross = li.querySelector(".cross");
+  let cross = li.querySelector(".cross");  //реакция нажатия на крестик
   cross.addEventListener('click', function () {
     li.remove();
-    localStorage.removeItem(obj.text);
+    let a = todos.indexOf(obj);
+    todos.splice(a,1)
+    filllocalStorage();
+    if (todos.length === 0) {
+      hidden.style.display = "none";
+    }
   })
-
   return li;
 };
 
 
 // заполнение списка элементами
-let renderTasks = () => {
+function renderTasks() {
   ul.innerHTML = "";
   todos.forEach((el) => ul.append(createTaskNode(el)));
   input.value =''
 };
 
 
-
 // загрузка элементов из localStorage при запуске страницы
+
 let start = () => {
-  if (localStorage.length !== 0) {
-    let keys = Object.keys(localStorage);
-    for (let key of keys) {
-      todos.unshift({
-        text: key,
-        id: Date.now(),
-        isCompleted: Boolean(+localStorage.getItem(key)),
-      });
-    };
+     if (localStorage.length !== 0) {
+   hidden.style.display = "block";
+   todos = JSON.parse(localStorage.getItem('todo'));
+   renderTasks();
+ }
+  };
+  start()
+
+  // реакция на нажатие кнопки "добавить"
+  add.addEventListener("click", function (event) {
+    event.preventDefault(); // отменить дефолтное поведение
     hidden.style.display = "block";
+    addTask();
     renderTasks();
-  } 
-};
-start()
+  });
 
 
 
-// реакция на нажатие кнопки "добавить"
-add.addEventListener("click", function (event) {
-     event.preventDefault();  // отменить дефолтное поведение
-  hidden.style.display="block"
-  addTask();
-  renderTasks();
-  
-  console.log(todos);
-});
-
-
-const deleteAll = document.querySelector(".delete-all");
 
 // реакция на нажатие кнопки "удалить все"
 deleteAll.addEventListener('click', function () {
@@ -105,24 +105,21 @@ deleteAll.addEventListener('click', function () {
 })
 
 
-const deleteCmpl = document.querySelector(".delete-cmpl");
-
 
 // реакция на нажатие кнопки "удалить завершенные"
 deleteCmpl.addEventListener('click', function () {
   ul.innerHTML = "";
-  let complete = todos.filter((el) => el.isCompleted === true);
-  complete.forEach((el) => {
-    if (localStorage.getItem(el.text) === "1") {
-      localStorage.removeItem(el.text);
-    }
-  });
-
   let newarr = todos.filter(el => el.isCompleted === false)
   newarr.forEach((el) => {
     ul.append(createTaskNode(el));
   });
   todos = newarr;
+
+  filllocalStorage();
+
+  if (todos.length ===0) {
+    hidden.style.display = "none";
+  }
 })
 
 
